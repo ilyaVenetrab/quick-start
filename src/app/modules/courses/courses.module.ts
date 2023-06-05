dd5c9b38bfd69015b158c4f81a6f151a8d50b302
Бартенев Илья <ibartenev@diasoft.ru>
2023-06-05 16:48:04 +0300

    module-1 - create core.module - create courses.module - create course, user interface

---
+++ src/app/modules/courses/courses.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoursesComponent } from './courses.component';
+
+@NgModule({
+  declarations: [CoursesComponent],
+  imports: [CommonModule],
+})
+export class CoursesModule {}
