04404dd510347a990350b8a083e25f77d0e8d27c
Бартенев Илья <ibartenev@diasoft.ru>
2023-06-28 13:59:57 +0300

    module-5

--- src/app/modules/core/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/modules/core/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BreadcrumbService } from '../../../../services/breadcrumb.service';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.sass'],
 })
-export class BreadcrumbsComponent {}
+export class BreadcrumbsComponent {
+  items: MenuItem[] = [];
+
+  home: MenuItem = {
+    icon: 'pi pi-home',
+    routerLink: '/',
+    label: 'Курсы',
+    iconStyle: { 'margin-right': '.5rem' },
+  };
+
+  constructor(private readonly breadcrumbService: BreadcrumbService) {
+    this.breadcrumbService.breadcrumbs$.subscribe((resp) => {
+      this.items = resp;
+    });
+  }
+}
